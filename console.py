aea399e1338ac9be9705e799be99423de8e90677
KananKarim <kenankerimli7770@gmail.com>
2024-03-01 18:27:36 +0400

    console version 0.0.1

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" console.py """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ hbnb """
+    prompt = '(hbnb) '
+    
+    def do_quit(self, arg):
+        """ Quit console """
+        return True
+    
+    def do_EOF(self, arg):
+        """ exit file with eof """
+        print("")
+        return True
+    
+    def emptyline(self):
+        """ empty line """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
